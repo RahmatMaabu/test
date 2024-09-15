80b8c1d1d9ba983490ffae738c57450f98ed0ff8
Calon Programmer <132484379+RahmatMaabu@users.noreply.github.com>
2024-09-15 13:18:08 +0800

    Update config.mjs

--- src/database/config/config.mjs
+++ src/database/config/config.mjs
@@ -1,26 +1,29 @@
-// eslint-disable-next-line import/prefer-default-export
-export const options = {
+const options = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     dialect: 'postgres',
-    logging: process.env.NODE_ENV === 'development'? console.log : false,
+    logging: process.env.NODE_ENV === 'development' ? console.log : false,
     migrationStorageTableName: 'migrations',
-}
+};
 
-if(process.env.NODE_ENV === 'production') {
-    options.dialectOptions = {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
+if (process.env.NODE_ENV === 'production') {
+    if (process.env.DATABASE_URL) {
+        options.url = process.env.DATABASE_URL;  // Use DATABASE_URL if available
+    } else {
+        options.dialectOptions = {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        };
     }
 }
 
 export default {
     development: options,
-    test: options,
-    production: options
-}
+    test: { ...options, logging: false },  // Disable logging in test environment
+    production: options,
+};
